6ee740e863b71d21b1b763728c338c613acc065e
ashwiniagre1 <ashwiniagre1>
2018-09-04 08:33:41 +0000

    migration n model added

---
+++ src/app/testnewdemo/core/models/mdoeltest.model.ts
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Date: 04/09/2018	
+ */
+export class Mdoeltest
+{
+	
+	private _id: string;
+	
+	constructor() { 
+	}
+	
+	set id(value: string) {
+		this._id = value;
+	}
+	get id() : string {
+		return this._id;
+	}
+	
+}
